01f1be7193456f6194b2719258a330977c283b69
Chen-Yiyang <iycspfa@gmail.com>
2020-12-29 14:37:08 +0800

    add styling for login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  get 'pages/my_todo_items'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+    root "pages#my_todo_items", as: :authenticated_root
+  end
+
+  root 'pages#home'
 end
