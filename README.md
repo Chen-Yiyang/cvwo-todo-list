69e5976a3559db28fc4990c0047dbba46be19943
Chen-Yiyang <iycspfa@gmail.com>
2021-01-07 10:33:00 +0800

    debounce update req

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # To-do Application
 - using Rails + React
   - frontend using React (View component of MVC design)
-- follow the tutorial at [here](https://stevepolito.design/blog/rails-react-tutorial/).
+- follow the tutorial at [here](https://stevepolito.design/blog/rails-react-tutorial/) for a basic Todo-list design.
 
 ## Notes
-- use Bootstrap for styling
-- use Devise gem for user login
-- use Axios for HTTP requests in React 
+- use `Bootstrap` for styling
+- use `Devise` gem for user login
+- use `Axios` for HTTP requests in React 
+- use `Lodash` for debouncing request in React
--- app/javascript/packs/components/TodoItem.jsx
+++ app/javascript/packs/components/TodoItem.jsx
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types'
 import axios from "axios";
 import setAxiosHeaders from "./AxiosHeaders";
 
+import _ from "lodash";
+
 class TodoItem extends React.Component {
     constructor(props) {
         super(props)
@@ -24,10 +26,15 @@ class TodoItem extends React.Component {
     }
 
     handleChange() {
+        this.setState({
+            complete: this.completedRef.current.checked
+        });
         this.updateTodoItem();
     }
-    updateTodoItem() {
-        this.setState({ complete: this.completedRef.current.checked });
+
+    // debounce update req.
+    // no req sent until no user inputs for 1 sec
+    updateTodoItem = _.debounce(() => {
         setAxiosHeaders();
         axios
             .put(this.path, {
@@ -40,7 +47,7 @@ class TodoItem extends React.Component {
             .catch(error => {
                 console.log(error);
             });
-    }
+    }, 1000);
 
     handleDestroy() {
         setAxiosHeaders();
@@ -55,7 +62,7 @@ class TodoItem extends React.Component {
                     console.log(error);
                 });
         }
-    }Z
+    }
 
     render() {
         const { todoItem } = this.props
